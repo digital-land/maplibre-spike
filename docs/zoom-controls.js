b68d60c1068fdf30f2a54f921ce4453126ffa025
Colm Britton <colmjude@gmail.com>
2021-06-28 11:19:43 +0100

    Remove commented out code

--- docs/zoom-controls.js
+++ docs/zoom-controls.js
@@ -1,61 +1,61 @@
 function ZoomControls ($module, leafletMap, initialZoom) {
-    this.$module = $module
-    this.map = leafletMap
-    this.initialZoom = initialZoom
-  }
-  
-ZoomControls.prototype.init = function (params) {
-this.setupOptions(params)
-
-if (!this.$module) {
-    return undefined
+  this.$module = $module
+  this.map = leafletMap
+  this.initialZoom = initialZoom
 }
-this.$module.classList.remove('js-hidden')
 
-const $buttons = this.$module.querySelectorAll('.' + this.buttonClass)
-this.$buttons = Array.prototype.slice.call($buttons)
+ZoomControls.prototype.init = function (params) {
+  this.setupOptions(params)
 
-this.$counter = this.$module.querySelector(this.counterSelector)
-this.$counter.textContent = this.initialZoom
+  if (!this.$module) {
+    return undefined
+  }
+  this.$module.classList.remove('js-hidden')
 
-const boundClickHandler = this.clickHandler.bind(this)
-this.$buttons.forEach(function ($button) {
+  const $buttons = this.$module.querySelectorAll('.' + this.buttonClass)
+  this.$buttons = Array.prototype.slice.call($buttons)
+
+  this.$counter = this.$module.querySelector(this.counterSelector)
+  this.$counter.textContent = this.initialZoom
+
+  const boundClickHandler = this.clickHandler.bind(this)
+  this.$buttons.forEach(function ($button) {
     $button.addEventListener('click', boundClickHandler)
-})
+  })
 
-const boundZoomHandler = this.zoomHandler.bind(this)
-// mapbox uses on not addEventListener
-this.map.on('zoomend', boundZoomHandler)
+  const boundZoomHandler = this.zoomHandler.bind(this)
+  // mapbox uses on not addEventListener
+  this.map.on('zoomend', boundZoomHandler)
 
-// get rid of default zoom controls
-//this.map.removeControl(this.map.zoomControl)
+  // get rid of default zoom controls
+  //this.map.removeControl(this.map.zoomControl)
 
-return this
+  return this
 }
 
 ZoomControls.prototype.clickHandler = function (e) {
-e.preventDefault()
-const $clickedEl = e.target
-let $clickedControl = $clickedEl
-// check if button was pressed
-// if contained span then find button
-if (!$clickedEl.classList.contains(this.buttonClass)) {
+  e.preventDefault()
+  const $clickedEl = e.target
+  let $clickedControl = $clickedEl
+  // check if button was pressed
+  // if contained span then find button
+  if (!$clickedEl.classList.contains(this.buttonClass)) {
     $clickedControl = $clickedEl.closest('.' + this.buttonClass)
-}
-this.zoom($clickedControl.dataset.zoomControl)
+  }
+  this.zoom($clickedControl.dataset.zoomControl)
 }
 
 ZoomControls.prototype.zoom = function (direction) {
-(direction === 'in') ? this.map.zoomIn(1) : this.map.zoomOut(1)
+  (direction === 'in') ? this.map.zoomIn(1) : this.map.zoomOut(1)
 }
 
 ZoomControls.prototype.zoomHandler = function (e) {
-const zoomLevel = this.map.getZoom()
-this.$counter.textContent = parseFloat(zoomLevel).toFixed(2)
+  const zoomLevel = this.map.getZoom()
+  this.$counter.textContent = parseFloat(zoomLevel).toFixed(2)
 }
 
 ZoomControls.prototype.setupOptions = function (params) {
-params = params || {}
-this.buttonClass = params.buttonClass || 'zoom-controls__btn'
-this.counterSelector = params.counterSelector || '.zoom-controls__count'
+  params = params || {}
+  this.buttonClass = params.buttonClass || 'zoom-controls__btn'
+  this.counterSelector = params.counterSelector || '.zoom-controls__count'
 }
